efab72c57f8e7ba919ffcd7b4bd0a877d86fff9d
lanyutao <ytlan0201@gmail.com>
2019-07-28 10:41:11 +0800

    add video driver

---
+++ os/drivers/screen.c
@@ -0,0 +1,115 @@
+#include "screen.h"
+#include "ports.h"
+
+
+int get_cursor();
+void set_cursor(int offset);
+int print_char(char c, int col, int row, char attr);
+int get_screen_offset(int col, int row);
+int get_offset_row(int offset);
+int get_offset_col(int offset);
+
+/*******************************************
+ * Public functions 
+ * *****************************************/
+
+void clear_screen() {
+	int row = 0;
+	int col = 0;
+	for (row = 0; row < MAX_ROWS; row++) {
+		for(col=0; col < MAX_COLS; col++) {
+			print_char(' ', col, row, WHITE_ON_BLACK);
+		}
+	}
+
+	set_cursor(get_screen_offset(0, 0));
+}
+
+void print_at(char *message, int col, int row) {
+	int offset;
+	if(col >= 0 && row >= 0) {
+		offset = get_screen_offset(col, row);
+	}else {
+		offset = get_cursor();
+		row = get_offset_row(offset);
+		col = get_offset_col(offset);
+	}
+
+	int i = 0;
+	while(message[i] != 0) {
+		offset = print_char(message[i++], col, row, WHITE_ON_BLACK);
+		row = get_offset_row(offset);
+		col = get_offset_col(offset);
+	}
+}
+
+void print(char *message) {
+	print_at(message, -1, -1);
+}
+
+/*******************************************
+ * Private functions 
+ * *****************************************/
+int print_char(char character, int col, int row, char attribute_byte) {
+	unsigned char *vidmem = (unsigned char *) VIDEO_ADDRESS;
+	
+	if (!attribute_byte) {
+		attribute_byte = WHITE_ON_BLACK;
+	}
+
+	if (col >= MAX_COLS || row >= MAX_ROWS) {
+		vidmem[2*(MAX_COLS)*(MAX_ROWS)-2] = 'E';
+		vidmem[2*(MAX_COLS)*(MAX_ROWS)-1] = RED_ON_WHITE;
+		return get_screen_offset(col, row);
+	}
+
+	int offset;
+	if (col >= 0 && row >= 0) {
+		offset	= get_screen_offset(col, row);
+	} else {
+		offset = get_cursor();
+	}
+
+	if (character == '\n') {
+		int rows = get_offset_row(offset);
+		offset = get_screen_offset(0, rows + 1);
+	} else {
+		vidmem[offset] = character;
+		vidmem[offset+1] = attribute_byte;
+		offset += 2;
+	}
+
+	set_cursor(offset);
+	return offset;
+}
+
+int get_cursor() {
+	// 读取光标地址, 询问VGA控制器寄存器0x3d4
+	// 14 = 光标高地址, 15 = 光标低地址
+	port_byte_out(REG_SCREEN_CTRL, 14);	
+	int offset = port_byte_in(REG_SCREEN_DATA) << 8;
+	port_byte_out(REG_SCREEN_CTRL, 15);
+	offset += port_byte_in(REG_SCREEN_DATA);
+	
+	// VGA'单元'由字符及其控制数据组成
+	// 例如'黑色背景上的白色'，'白色背景上的红色文字'等
+	return offset * 2;
+}
+
+void set_cursor(int offset) {
+	offset /= 2;
+	port_byte_out(REG_SCREEN_CTRL, 14);
+	port_byte_out(REG_SCREEN_DATA, (unsigned char)(offset >> 8));
+	port_byte_out(REG_SCREEN_CTRL, 15);
+	port_byte_out(REG_SCREEN_DATA, (unsigned char)(offset & 0xff));
+}
+
+int get_screen_offset(int col, int row) { 
+	return 2 * (row * MAX_COLS + col); 
+}
+int get_offset_row(int offset) { 
+	return offset / (2 * MAX_COLS); 
+}
+int get_offset_col(int offset) { 
+	return (offset - (get_offset_row(offset)*2*MAX_COLS))/2; 
+}
--- os/kernel/kernel.c
+++ os/kernel/kernel.c
@@ -1,4 +1,5 @@
-#include "../drivers/ports.h"
+#include "../drivers/screen.h"
+
 void some_func() {
 	char *video_memory = (char *)0xb8002;  // 屏幕地址
 	*video_memory = 'O'; 	      // 左上角显示字符
@@ -12,19 +13,30 @@ int main() {
 
 	// 读取光标地址, 询问VGA控制器寄存器0x3d4
 	// 14 = 光标高地址, 15 = 光标低地址
-	port_byte_out(0x3d4, 14);
+	//port_byte_out(0x3d4, 14);
 
-	int position = port_byte_in(0x3d5);
-	position = position << 8;
+	//int position = port_byte_in(0x3d5);
+	//position = position << 8;
 
-	port_byte_out(0x3d4, 15);
-	position += port_byte_in(0x3d5);
+	//port_byte_out(0x3d4, 15);
+	//position += port_byte_in(0x3d5);
 
-	// VGA'单元'由字符及其控制数据组成
-	// 例如'黑色背景上的白色'，'白色背景上的红色文字'等
-	int offset_from_vga = position * 2;
+	//// VGA'单元'由字符及其控制数据组成
+	//// 例如'黑色背景上的白色'，'白色背景上的红色文字'等
+	//int offset_from_vga = position * 2;
 
-	char *vga = (char *)0xb8000;
-	vga[offset_from_vga] = 'X';
-	vga[offset_from_vga+1] = 0x0f;
+	//char *vga = (char *)0xb8000;
+	//vga[offset_from_vga] = 'X';
+	//vga[offset_from_vga+1] = 0x0f;
+	
+	clear_screen();
+	print_at("X", 0, 0);
+	print_at("X", 1, 1);
+	print_at("E", MAX_COLS - 1, MAX_ROWS - 1);
+	print_at("hello world!", 0, 2);
+	print_at("This text spans multiple lines", 75, 10);
+	print_at("?", -1, -1);
+	print_at("There is a line\nbreak", 0, 20);
+	print("!!There is a line\nbreak");
+	print_at("What happens when we run out of space?", 45, 24);
 }
