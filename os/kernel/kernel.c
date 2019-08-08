5b75f7ff0290e9700abd1cb95d89b252d6a62260
lanyutao <ytlan0201@gmail.com>
2019-08-08 14:00:14 +0800

    add kmalloc

--- os/kernel/kernel.c
+++ os/kernel/kernel.c
@@ -5,6 +5,7 @@
 #include "../cpu/timer.h"
 #include "../drivers/keyboard.h"
 #include "../cpu/ports.h"
+#include "../libc/mem.h"
 
 void some_func() {
 	char *video_memory = (char *)0xb8000;  // 屏幕地址
@@ -92,8 +93,20 @@ void user_input(char *input) {
 	if(strcmp(input, "END") == 0) {
         print("Stopping the CPU. Bye!\n");
         asm volatile("hlt");
-    }
-    
+    } else if (strcmp(input, "PAGE") == 0) {
+        u32 phys_addr;
+        u32 page = kmalloc(1, 1, &phys_addr);
+        char page_str[16] = "";
+        hx_to_ascii(page, page_str);
+        char phys_str[16] = "";
+        hx_to_ascii(phys_addr, phys_str);
+        
+        print("Page: ");
+        print(page_str);
+        print(", physical address: ");
+        print(phys_str);
+        print("\n");
+    }    
     print("You said: ");
 	print(input);
 	print("\n> ");
